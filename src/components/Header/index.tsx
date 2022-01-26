301cc058ec66bd57ce7c77a8d5987a69fe221a1b
Garoze <marcelogaroze@hotmail.com>
2022-01-25 21:05:49 -0300

    feat: Header component and styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,22 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.headerContainer}>
+      <Link href="/" passHref>
+        <a>
+          <Image
+            priority
+            src="/images/logo.svg"
+            width={238}
+            height={25}
+            alt="logo"
+          />
+        </a>
+      </Link>
+    </header>
+  );
 }
